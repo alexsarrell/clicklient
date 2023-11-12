e637d7ee1e9ed08b69079ef0b6f1c3740b79d29e
sunnyshechka <alexsarrell.workspace@gmail.com>
2023-11-12 22:40:57 +0300

    init

---
+++ src/test/java/com/aalpov/opendbadapter/OpenDbAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aalpov.opendbadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDbAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
